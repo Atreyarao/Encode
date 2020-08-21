5b0ab53066e4c895751873b59aac29dd05789b31
Atreyarao <47099164+Atreyarao@users.noreply.github.com>
2020-08-21 19:19:58 +0530

    Updated Encode.js
    New Algorithm

--- utils/encode.js
+++ utils/encode.js
@@ -3,29 +3,53 @@ const route = express.Router();
 
 route.post("/encode", (req, res) => {
     var str = req.body.str;
-    console.log(req.body);
-    if (str.toLowerCase() !== str) res.json(-1);
-    var n = str.length;
-    var ans = "";
+    // console.log(str)
+    if (str !== str.toLowerCase()) {
+        res.send("-1");
+        return;
+    }
+    var result = 0;
     var i = 0;
-    while (true) {
-        if (i > n) break;
-        var char = str.charAt(i);
-        if (char >= '0' && char <= '9') res.json(-2);
-        var count = 1;
-        var j = i + 1;
-        while (true) {
-            if (j > n) break;
-            var ch = str.charAt(j);
-            if (char !== ch) break;
-            count++;
-            j++;
+    var count = str.length - 1;
+    for (i = 0; i < str.length; i++) {
+        var ch = str.charAt(i);
+        if (ch >= '0' && ch <= '9') { res.status(400).send("-2"); return }
+        var temp = 1;
+        for (var j = 0; j < count; j++) temp *= 26;
+        count--;
+        if (ch >= 'a' && ch <= 'z') {
+            var diff = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+            diff++;
+            temp *= diff;
+        } else {
+            var diff = ch.charCodeAt(0) - 'A'.charCodeAt(0);
+            temp *= diff;
         }
-        if (count === 1) ans += char; else ans += char + count;
-        i = i + count;
+        result += temp;
+    }
+    console.log(result);
+    var b52 = "";
+
+    while (result > 0) {
+        var mod = result % 52;
+        if (mod < 26) {
+            var diff = "a".charCodeAt(0);
+            diff += mod;
+            diff = String.fromCharCode(diff)
+            b52 += diff;
+        } else {
+            var diff = "A".charCodeAt(0);
+            mod -= 26;
+            diff += mod;
+
+            diff = String.fromCharCode(diff)
+            b52 += diff;
+        }
+        result = Math.floor(result / 52);
     }
 
-    res.send(ans);
+    b52 = b52.split('').reverse().join('')
+    res.send(b52);
 
 })
 
